9239d18145d34e511f232399b8f36e2cc5c7253b
Michele Mostoller <MMostoller@att.net>
2024-06-07 15:22:37 -0400

    Exercise 1

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            decimal sum = num1 + num2;
+            return sum;
+        }
+
+        public static string Add(int num1, int num2, bool CanShowDollar)
+        {
+            string dollars = num1 + num2 == 1 ? "dollar" : "dollars";
+            return CanShowDollar ? $"{num1 + num2} {dollars}" : $"{num1 + num2}";
+        
+        }           
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(0, 1, true));
+            Console.WriteLine(Add(3, 6));
+            Console.WriteLine(Add(3.2m, 5.4m));
         }
     }
 }
